594c6f5e9b9f6e5dbad67503c52703e7f3b8f024
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Pro.local>
2019-12-05 17:34:03 +0530

    Try to make js work

---
+++ dist/some.js
@@ -0,0 +1,104 @@
+(function(window){
+    window.extractData = function() {
+        var ret = $.Deferred();
+
+        function onError() {
+            console.log('Loading error', arguments);
+            ret.reject();
+        }
+
+        function onReady(smart)  {
+            if (smart.hasOwnProperty('patient')) {
+                var patient = smart.patient;
+                var pt = patient.read();
+                var obv = smart.patient.api.fetchAll({
+                    type: 'Observation',
+                    query: {
+                        code: {
+                            $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                                'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                                'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        }
+                    }
+                });
+
+                $.when(pt, obv).fail(onError);
+
+                $.when(pt, obv).done(function(patient, obv) {
+                    var byCodes = smart.byCodes(obv, 'code');
+                    var gender = patient.gender;
+
+                    var fname = '';
+                    var lname = '';
+
+                    if (typeof patient.name[0] !== 'undefined') {
+                        fname = patient.name[0].given.join(' ');
+                        lname = patient.name[0].family.join(' ');
+                    }
+
+
+                    var p = defaultPatient();
+                    p.birthdate = patient.birthDate;
+                    p.gender = gender;
+                    p.fname = fname;
+                    p.lname = lname;
+
+                    ret.resolve(p);
+                });
+            } else {
+                onError();
+            }
+        }
+
+        FHIR.oauth2.ready(onReady, onError);
+        return ret.promise();
+
+    };
+
+    function defaultPatient(){
+        return {
+            fname: {value: ''},
+            lname: {value: ''},
+            gender: {value: ''},
+            birthdate: {value: ''},
+        };
+    }
+
+    function getBloodPressureValue(BPObservations, typeOfPressure) {
+        var formattedBPObservations = [];
+        BPObservations.forEach(function(observation){
+            var BP = observation.component.find(function(component){
+                return component.code.coding.find(function(coding) {
+                    return coding.code == typeOfPressure;
+                });
+            });
+            if (BP) {
+                observation.valueQuantity = BP.valueQuantity;
+                formattedBPObservations.push(observation);
+            }
+        });
+
+        return getQuantityValueAndUnit(formattedBPObservations[0]);
+    }
+
+    function getQuantityValueAndUnit(ob) {
+        if (typeof ob != 'undefined' &&
+            typeof ob.valueQuantity != 'undefined' &&
+            typeof ob.valueQuantity.value != 'undefined' &&
+            typeof ob.valueQuantity.unit != 'undefined') {
+            return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+        } else {
+            return undefined;
+        }
+    }
+
+    window.drawVisualization = function(p) {
+        console.log("Hey");
+        console.log(p.fname);
+        $('#fname').text(p.fname);
+        $('#lname').text(p.lname);
+        $('#gender').text(p.gender);
+        $('#birthdate').text(p.birthdate);
+    };
+
+})(window);
